92253357b2bd1062775cf7d3eaaa09fed4d351db
jacoborous <59513046+jacoborous@users.noreply.github.com>
2020-02-01 11:11:54 -0800

    project

--- formal/.stack-work/dist/e626a42b/build/autogen/Paths_formal_types.hs
+++ formal/.stack-work/dist/e626a42b/build/autogen/Paths_formal_types.hs
@@ -29,12 +29,12 @@ version :: Version
 version = Version [0,0,0,1] []
 bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "D:\\sources\\haskell\\.stack-work\\install\\b4553662\\bin"
-libdir     = "D:\\sources\\haskell\\.stack-work\\install\\b4553662\\lib\\x86_64-windows-ghc-8.6.5\\formal-types-0.0.0.1-LNuWVcA3B6I9n93ajhqv14"
-dynlibdir  = "D:\\sources\\haskell\\.stack-work\\install\\b4553662\\lib\\x86_64-windows-ghc-8.6.5"
-datadir    = "D:\\sources\\haskell\\.stack-work\\install\\b4553662\\share\\x86_64-windows-ghc-8.6.5\\formal-types-0.0.0.1"
-libexecdir = "D:\\sources\\haskell\\.stack-work\\install\\b4553662\\libexec\\x86_64-windows-ghc-8.6.5\\formal-types-0.0.0.1"
-sysconfdir = "D:\\sources\\haskell\\.stack-work\\install\\b4553662\\etc"
+bindir     = "C:\\Users\\Tristan\\IdeaProjects\\formal-types\\.stack-work\\install\\d20d4f73\\bin"
+libdir     = "C:\\Users\\Tristan\\IdeaProjects\\formal-types\\.stack-work\\install\\d20d4f73\\lib\\x86_64-windows-ghc-8.6.5\\formal-types-0.0.0.1-LNuWVcA3B6I9n93ajhqv14"
+dynlibdir  = "C:\\Users\\Tristan\\IdeaProjects\\formal-types\\.stack-work\\install\\d20d4f73\\lib\\x86_64-windows-ghc-8.6.5"
+datadir    = "C:\\Users\\Tristan\\IdeaProjects\\formal-types\\.stack-work\\install\\d20d4f73\\share\\x86_64-windows-ghc-8.6.5\\formal-types-0.0.0.1"
+libexecdir = "C:\\Users\\Tristan\\IdeaProjects\\formal-types\\.stack-work\\install\\d20d4f73\\libexec\\x86_64-windows-ghc-8.6.5\\formal-types-0.0.0.1"
+sysconfdir = "C:\\Users\\Tristan\\IdeaProjects\\formal-types\\.stack-work\\install\\d20d4f73\\etc"
 
 getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "formal_types_bindir") (\_ -> return bindir)
