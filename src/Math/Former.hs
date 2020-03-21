cdfc421222dfb6ddcef022a52583105b6d9a5975
jacoborous <jacobeus@protonmail.com>
2020-03-21 21:42:18 +0000

    match type

--- src/Math/Former.hs
+++ src/Math/Former.hs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -60,67 +59,4 @@ form (Equals x y) = DefEq x y
 
 instance Show a => Show (Former a) where
     show a = show $ form a
-=======
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE RankNTypes #-}
-
-module Math.Former where
-
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Tree (Tree)
-import qualified Data.Tree as Tree
-import qualified Data.Map.Strict as Map
-import Control.Applicative
-import Data.Semigroup
-import Data.List
-import Math.Util
-import Math.Term
-import Math.Context
-
-data Former a where
-    NewType :: String -> Former Term
-    C :: String -> [Term] -> Former Term
-    Set :: Term -> [Term] -> Former Term
-    Variable :: String -> Term -> Former Term
-    Apply :: Context (Tree.Tree Term) -> Term -> [Term] -> Former Term
-    Function :: Term -> Term -> Former Term
-    Family :: String -> Term -> Term -> Former Term
-    ForAll :: String -> Term -> Term -> Former Term
-    ThereExists :: String -> Term -> Term -> Former Term
-    Bind :: Term -> Term -> Former Term
-    Subst :: Term -> Term -> Term -> Former Term
-    BetaReduce :: Term -> Former Term
-    AlphaReduce :: Term -> Former Term
-    Equals :: Term -> Term -> Former Term
-
-
-form :: Former a -> a
-form (NewType s) = cnst s
-form (C s xs) = foldl (.$) (cnst s) xs
-form (Set a b) = Def a b
-form (Variable s t) = Var s t
-form (Function a b) = if Set.disjoint (freeVars a) (freeVars b) then Pi a b else error $ "Cannot construct the function type: type " ++ show b ++ " depends on elements from type " ++ show a
-form (Family x a b) = Ap b (Var x a)
-form (ForAll x a b) = Pi (Var x a) (Ap b (Var x a))
-form (ThereExists x a b) = Sigma a (beta $ (form $ ForAll x a b) .$ a) --elim
-form (Bind x a) = bind x a
-form (Subst e r m) = substitution m e r
-form (Apply ctx f args) = go (arity f) args where
-  go [] [] = f
-  go (m:ms) (x:xs) = if compare2 ctx x m == Just SUBTYPE then form (Apply ctx (beta $ f .$ x) xs) else error $ "Illegal argument exception: " ++ show m ++ ", given: " ++ show x
-  go [] xs = error "Too many arguments given."
-  go ms [] = error "Too few arguments given."
-form (BetaReduce t) = betaReduce t
-form (AlphaReduce t) = alphaReduce t
-form (Equals x y) = DefEq x y
-
-instance Show a => Show (Former a) where
-    show a = show $ form a
->>>>>>> b5b58b34cdfda8e83a32778ee25ecd15633af83b
     
--- src/Math/Induction/Induction.hs
+++ src/Math/Induction/Induction.hs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {-# LANGUAGE GADTs #-}
 
 module Math.Induction.Induction where
@@ -32,38 +31,3 @@ computeBeta ctx = intros ctx (go (typeMatch ctx a)) where
     a = Ap (Pi U U) U
     go [] = []
     go (x:xs) = if x /= beta x then (Ident x (beta x)) : go xs else go xs
-=======
-{-# LANGUAGE GADTs #-}
-
-module Math.Induction.Induction where
-
-import Math.Context
-import Math.Term
-import Math.Former
-import Math.Util
-import Math.Induction.TypeMatch
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Tree (Tree)
-import qualified Data.Tree as Tree
-
-data Inductor = Inductor [(Pattern, Term -> Term)]
-
-prove :: Former Term -> Context (Tree.Tree Term) -> Context (Tree.Tree Term)
-prove (C s xs) ctx = if and (fmap (exists ctx) xs) then intro ctx (form $ C s xs) else error "Cannot form, some types do not exist."
-prove (Family x a b) ctx = if isTrue ctx a then intros ctx ((Def (Pi a U) [b]) : (fmap (b .$) (shallowMatch ctx (Var x a)))) else intro ctx (Pi (form (Family x a b)) (Prim Zero))
-prove (Set a xs) ctx = intro ctx (form (Set a xs))
-prove (NewType x) ctx = intro ctx (cnst x)
-prove (ForAll x a b) ctx = if isTrue ctx a then intros ctx ((Def (Pi a U) [b]) : (fmap (Ap (form (ForAll x a b))) (shallowMatch ctx (Var x a)))) else intro ctx (Pi (form (ForAll x a b)) (Prim Zero))
-prove (Equals a b) ctx
-    | exists ctx a && exists ctx b = if (typeOf ctx a == typeOf ctx b) then intro ctx (Def (Ident a b) [form (Equals a b)]) else error $ "terms already exist and are not the same type: " ++ show (typeOf ctx a) ++ ", " ++ show (typeOf ctx b)
-    | exists ctx a = intros ctx [Def (last $ typeOf ctx a) [b], (Def (Ident a b) [form (Equals a b)])]
-    | exists ctx b = intros ctx [Def (last $ typeOf ctx b) [a], (Def (Ident a b) [form (Equals a b)])]
-    | otherwise = intros ctx [a, b, (Def (Ident a b) [form (Equals a b)])]
-
-computeBeta :: Context (Tree.Tree Term) -> Context (Tree.Tree Term)
-computeBeta ctx = intros ctx (go (typeMatch ctx a)) where
-    a = Ap (Pi U U) U
-    go [] = []
-    go (x:xs) = if x /= beta x then (Ident x (beta x)) : go xs else go xs
->>>>>>> b5b58b34cdfda8e83a32778ee25ecd15633af83b
